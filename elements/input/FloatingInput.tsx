b44a627f9c5cde67105fb0f3e6ac938047a8423a
Jonas Strehle <jonas@strehles.info>
2025-05-28 00:40:10 +0200

    Fix link

--- elements/input/FloatingInput.tsx
+++ elements/input/FloatingInput.tsx
@@ -1,6 +1,9 @@
 import { blankTemplate } from "uix/html/template.ts"
 
-export const FloatingInput = blankTemplate<Partial<HTMLInputElement>>(({placeholder, ...props}) => 
+export const FloatingInput = blankTemplate<Partial<HTMLInputElement> & {
+	minlength?: number;
+	maxlength?: number;
+}>(({placeholder, ...props}) => 
 <div class="input-container" stylesheet="./FloatingInput.css?" >
 	<span>{placeholder}</span>
 	<input 
